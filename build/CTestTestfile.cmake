2b61d142b9c4e2dee3641d24584642154c4fa896
ka1te6 <a.andrey2006@yandex.ru>
2025-05-28 05:17:32 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ka1te/ka1te6/workspace/projects/rk2
-# Build directory: /home/ka1te/ka1te6/workspace/projects/rk2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ka1te/ka1te6/workspace/projects/rk2/tests
-# Build directory: /home/ka1te/ka1te6/workspace/projects/rk2/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(BridgeTests "/home/ka1te/ka1te6/workspace/projects/rk2/build/tests/BridgeTests")
-set_tests_properties(BridgeTests PROPERTIES  _BACKTRACE_TRIPLES "/home/ka1te/ka1te6/workspace/projects/rk2/tests/CMakeLists.txt;16;add_test;/home/ka1te/ka1te6/workspace/projects/rk2/tests/CMakeLists.txt;0;")
